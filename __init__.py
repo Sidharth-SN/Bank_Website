d82e560c11418cb9266aaa0d407ed502dced26c2
Sidharth S N <75628665+Sidharth-SN@users.noreply.github.com>
2022-11-07 15:08:58 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.secret_key ='sdfjosidjh4ojg$_%JOI)JOJ54469420@69'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wbslyaejyfjamr:cd7970d4b3cc3f721288af4c5cda5293054e0e092e0d7014c2086844b5f72c63@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d69rgo10i23arq'
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wbslyaejyfjamr:cd7970d4b3cc3f721288af4c5cda5293054e0e092e0d7014c2086844b5f72c63@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d69rgo10i23arq'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
