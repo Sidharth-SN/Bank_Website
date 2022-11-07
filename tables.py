f6a5e38bfef97c4ee8e44b91c955ad9bc814395e
Sidharth S N <75628665+Sidharth-SN@users.noreply.github.com>
2022-11-07 16:11:24 +0530

    Update tables.py

--- tables.py
+++ tables.py
@@ -11,15 +11,15 @@ class Users(db.Model):
     passwd = db.Column(db.String(100), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.now)
 
-    banks = db.relationship('Accounts', backref = 'users', passive_deletes = True)
+    banks = db.relationship('Accounts', backref = 'users')
 
     # def __repr__(self):
     #     return f'{self.sno} : {self.username} - {self.email}'
 
 class Accounts(db.Model):
-    acc_no = db.Column(db.Integer, primary_key = True)
+    acc_no = db.Column(db.BIGINT, primary_key = True)
     ifsc = db.Column(db.String(20), nullable = False)
-    balance = db.Column(db.Integer, default = 0)
+    balance = db.Column(db.BIGINT, default = 0)
     acc_holder_name = db.Column(db.String(50), nullable = False)
     father_name =  db.Column(db.String(50), nullable = False)
     dob = db.Column(db.Date, nullable = False)
@@ -27,9 +27,9 @@ class Accounts(db.Model):
     state = db.Column(db.String(20), nullable = False)
     pin_code = db.Column(db.Integer, nullable = False)
     country = db.Column(db.String(20), nullable = False)
-    phoneNo = db.Column(db.Integer, nullable = False)
+    phoneNo = db.Column(db.BIGINT, nullable = False)
     occupation = db.Column(db.String(100))
-    aadhar_no = db.Column(db.Integer, nullable = False)
+    aadhar_no = db.Column(db.BIGINT, nullable = False)
     pan_no = db.Column(db.String(20), nullable = False)
     marital = db.Column(db.String(10))
     sex = db.Column(db.String(7), nullable = False)
@@ -37,14 +37,14 @@ class Accounts(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete = 'CASCADE'))
     
-    c_card = db.relationship('Credit_card', backref = 'cacc', passive_deletes = True)
-    d_card = db.relationship('Debit_card', backref = 'dacc', passive_deletes = True)
-    t_acc = db.relationship('Transactions', backref = 'trans', passive_deletes = True)
-    loan = db.relationship('Loans', backref = 'loans', passive_deletes = True)
+    c_card = db.relationship('Credit_card', backref = 'cacc')
+    d_card = db.relationship('Debit_card', backref = 'dacc')
+    t_acc = db.relationship('Transactions', backref = 'trans')
+    loan = db.relationship('Loans', backref = 'loans')
         
 
 class Credit_card(db.Model):
-    card_no = db.Column(db.Integer, primary_key = True)
+    card_no = db.Column(db.BIGINT, primary_key = True)
     card_holder_name = db.Column(db.String(50), nullable = False)
     credit_limit = db.Column(db.Integer, nullable = False)
     outstanding = db.Column(db.Integer, default = 0)
@@ -57,7 +57,7 @@ class Credit_card(db.Model):
 
 
 class Debit_card(db.Model):
-    card_no = db.Column(db.Integer, primary_key = True)
+    card_no = db.Column(db.BIGINT, primary_key = True)
     card_holder_name = db.Column(db.String(50), nullable = False)
     # balance = db.Column(db.Integer, default = 0)
     valid_from = db.Column(db.String(7), nullable = False)
@@ -69,10 +69,10 @@ class Debit_card(db.Model):
 
 class Transactions(db.Model):
     sno = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    tid = db.Column(db.Integer, nullable = False)
+    tid = db.Column(db.BIGINT, nullable = False)
     dot = db.Column(db.DateTime, nullable = False, default = datetime.now)
-    tAmount = db.Column(db.Integer, default = 0)
-    acc_to = db.Column(db.Integer, nullable = False)
+    tAmount = db.Column(db.BIGINT, default = 0)
+    acc_to = db.Column(db.BIGINT, nullable = False)
     medium_tranc = db.Column(db.String(10), nullable = False, default = 'Online')
     type_tranc = db.Column(db.String(10), nullable = False)
 
@@ -80,16 +80,16 @@ class Transactions(db.Model):
 
 
 class Loans(db.Model):
-    lid = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    lid = db.Column(db.BIGINT, primary_key = True, autoincrement = True)
     full_name  = db.Column(db.String(20), nullable = False)
     father_name = db.Column(db.String(20), nullable = False)
-    phone = db.Column(db.Integer, nullable = False)
+    phone = db.Column(db.BIGINT, nullable = False)
     address = db.Column(db.String(100), nullable = False)
     age = db.Column(db.Integer, nullable = False)
     proprietor = db.Column(db.String(20), nullable = False)
     occupation = db.Column(db.String(30), nullable = False)
     nationality = db.Column(db.String(20), nullable = False)
-    income = db.Column(db.Integer, nullable = False)
+    income = db.Column(db.BIGINT, nullable = False)
     loan_purpose = db.Column(db.String(40), nullable = False)
     loan_type = db.Column(db.String(20), nullable = False)
     loan_intrest = db.Column(db.String(20), nullable = False)
